6fc29de204621663dc76f77e5e909a6130a1e7cb
Emmanuelle <emmanuellematos9@gmail.com>
2023-05-12 15:59:29 -0300

    WIP: criando formulario de post

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,62 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  data () {
+    return {
+      posts: [
+        {
+          title: 'Meu Primeiro Post',
+        datetime: Date.now(),
+        content: 'Postar aqui é muito legal',
+      },
+
+      {title: 'Meu Segundo Post',
+        datetime: Date.now(),
+        content: 'Postar aqui não é muito legal',
+
+        }
+      ]
+    }
+  },
+  methods: {
+    handleSubmit (event) {
+      event.preventDefault()
+    }
+  }
+}
+
 </script>
 
 <template>
 
+  <div id="lista-posts">
+    <div class="post" v-for="post in posts" :key="post.title">
+      <h3>{{ post.title }}</h3>
+      <h4>{{ post.datetime }}</h4>
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+
+
+  <form action="">
+    <input name="title">
+    <textarea name="content" id="" cols="30" rows="10"></textarea>
+    <button type="submit">Criar</button>
+  </form>
 
   <RouterView />
 </template>
 
 <style scoped>
 
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+form > * {
+  margin: 1rem;
+}
 
 </style>
