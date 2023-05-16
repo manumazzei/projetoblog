18b88c1a4ad635e949175b662855ed448f65cf12
Emmanuelle <emmanuellematos9@gmail.com>
2023-05-16 10:52:22 -0300

    finaliza criação de post

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,96 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 
 export default {
-  data () {
+  data() {
     return {
       posts: [
         {
-          title: 'Meu Primeiro Post',
-        datetime: Date.now(),
-        content: 'Postar aqui é muito legal',
+          title: "Meu Primeiro Post",
+          datetime: Date.now(),
+          content: "Postar aqui é muito legal",
+        },
+
+        {
+          title: "Meu Segundo Post",
+          datetime: Date.now(),
+          content: "Postar aqui não é muito legal",
+        },
+      ],
+      formData: {
+        title: "",
+        content: "",
       },
-
-      {title: 'Meu Segundo Post',
-        datetime: Date.now(),
-        content: 'Postar aqui não é muito legal',
-
-        }
-      ]
-    }
+    };
   },
   methods: {
-    handleSubmit (event) {
-      event.preventDefault()
-    }
-  }
-}
+    handleClick(event) {
+      /* adicionar o post a lista de posts */
+      const now = new Date()
 
+      const dataDaPostagem = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
+
+      this.posts.push({
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      });
+
+      this.formData = {
+        name:"",
+        content: "",
+      };
+    },
+    handleImputChange(event) {
+      const { name, value } = event.target;
+      this.formData[name] = value;
+    },
+  },
+};
 </script>
 
 <template>
-
   <div id="lista-posts">
-    <div class="post" v-for="post in posts" :key="post.title">
+    <div v-for="post in posts" :key="post.key">
       <h3>{{ post.title }}</h3>
       <h4>{{ post.datetime }}</h4>
       <p>{{ post.content }}</p>
     </div>
   </div>
 
+  <form>
+    <input v-model="formData.title" placeholder="Titulo"/>
+    <textarea
+      name="content"
+      :value="formData.content"
+      @keyup="handleImputChange"
+      placeholder="Escreva seu post aqui..."
+      id=""
+      cols="30"
+      rows="10"
+    ></textarea>
 
-  <form action="">
-    <input name="title">
-    <textarea name="content" id="" cols="30" rows="10"></textarea>
-    <button type="submit">Criar</button>
+    <button type="button" @click="handleClick">Salvar</button>
   </form>
 
   <RouterView />
 </template>
 
 <style scoped>
-
 form {
   display: flex;
   flex-direction: column;
+  border: 2px solid #35030B;
 }
 
 form > * {
   margin: 1rem;
 }
 
+#lista-posts {
+  background: #ECDCCE;
+  color: #35030B;
+  padding: 2rem 2rem;
+  font-family: Impact;
+}
 </style>
