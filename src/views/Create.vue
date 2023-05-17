0f6eb2e46ffe21ea896a408ca77695587cdb5fb0
Emmanuelle <emmanuellematos9@gmail.com>
2023-05-17 15:11:24 -0300

    implementa criação de posts em nova view

---
+++ src/views/Create.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  methods: {
+    handleClick(event) {
+
+      if(!this.formData.title) {
+        alert("preencha o título do post");
+        return;
+      }
+
+      /* adicionar o post a lista de posts */
+      const now = new Date();
+
+      const dataDaPostagem = `${now.getDate()}/${
+        now.getMonth() + 1
+      }/${now.getFullYear()}`;
+
+     /*  this.posts.push({
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      });  */
+
+      const newPost={
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      };
+
+      /* emitir o evento create-post */
+      this.$emit("create-post", newPost);
+
+      this.formData = {
+        name: "",
+        content: "",
+      };
+    },
+
+    handleImputChange(event) {
+      const { name, value } = event.target;
+      this.formData[name] = value;
+    },
+  },
+};
+</script>
+
+<template>
+  <form>
+    <input v-model="formData.title" placeholder="Titulo" />
+    <textarea
+      name="content"
+      :value="formData.content"
+      @keyup="handleImputChange"
+      placeholder="Escreva seu post aqui..."
+      id=""
+      cols="30"
+      rows="10"
+    ></textarea>
+
+    <button type="button" @click="handleClick">Salvar</button>
+  </form>
+</template>
